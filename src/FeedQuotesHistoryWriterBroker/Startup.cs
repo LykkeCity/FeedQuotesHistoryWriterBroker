5ed34df5e4b6553c6760419d7c1f6db8fc3c88bd
KonstantinRyazantsev <kj.ryazancev@gmail.com>
2017-09-27 19:26:14 +0300

    Lykke.Logs updated

--- src/FeedQuotesHistoryWriterBroker/Startup.cs
+++ src/FeedQuotesHistoryWriterBroker/Startup.cs
@@ -150,17 +150,13 @@ namespace FeedQuotesHistoryWriterBroker
             // Creating azure storage logger, which logs own messages to concole log
             if (!string.IsNullOrEmpty(dbLogConnectionString) && !(dbLogConnectionString.StartsWith("${") && dbLogConnectionString.EndsWith("}")))
             {
-                const string appName = "FeedQuotesHistoryWriterBroker";
-
                 var persistenceManager = new LykkeLogToAzureStoragePersistenceManager(
-                    appName,
                     AzureTableStorage<LogEntity>.Create(dbLogConnectionStringManager, "FeedQuotesHistoryWriterBrokerLogs", consoleLogger),
                     consoleLogger);
 
-                var slackNotificationsManager = new LykkeLogToAzureSlackNotificationsManager(appName, slackService, consoleLogger);
+                var slackNotificationsManager = new LykkeLogToAzureSlackNotificationsManager(slackService, consoleLogger);
 
                 var azureStorageLogger = new LykkeLogToAzureStorage(
-                    appName,
                     persistenceManager,
                     slackNotificationsManager,
                     consoleLogger);
