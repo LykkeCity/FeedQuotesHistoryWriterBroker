91d869ef03e5de96808f6144496de0dd89467637
Mikhail Gerasimov <mikhail.gerasimov@lykke.com>
2017-11-16 20:09:28 +0100

    Rename to Lykke.Service.QuotesHistory

--- src/FeedQuotesHistoryWriterBroker/Models/ErrorResponse.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FeedQuotesHistoryWriterBroker.Models
-{
-    public class ErrorResponse
-    {
-        public string ErrorMessage { get; set; }
-    }
-}
---
+++ src/Lykke.Service.QuotesHistory.Repositories/QuoteItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Lykke.Domain.Prices.Contracts;
+using Newtonsoft.Json;
+
+namespace Lykke.Service.QuotesHistory.Repositories
+{
+    public class QuoteItem : IQuote
+    {
+        [JsonProperty("A")]
+        public string AssetPair { get; set; }
+        [JsonProperty("B")]
+        public bool IsBuy { get; set; }
+        [JsonProperty("P")]
+        public double Price { get; set; }
+        [JsonProperty("T")]
+        public DateTime Timestamp { get; set; }
+    }
+}
